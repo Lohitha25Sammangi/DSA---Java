703a016f8d6fd20e2a05802a1833e22b18155b06
LohithaSammagi <lohithasammangi12@gmail.com>
2025-06-27 14:20:44 +0530

    Add files via upload

---
+++ strings/Main.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        String name = "Kunal Kushwaha";
+        System.out.println(name);
+
+        String a = "Kunal";
+        String[] b=a.split("n");
+        System.out.println(Arrays.toString(b));
+        System.out.println(a);
+        a = "Kushwaha";
+        System.out.println(a);
+    }
+}
---
+++ strings/Output.java
@@ -0,0 +1,16 @@
+package strings;
+import java.util.*;
+public class Output {
+    public static void main(String[] args) {
+        int a=23;
+        System.out.println(Integer.toString(a));
+        System.out.println(56);
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+        System.out.println(num);
+        System.out.println("Kunal");
+        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+        String name = null;
+        System.out.println(name);
+    }
+}
